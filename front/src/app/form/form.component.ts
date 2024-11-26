41b001af208e9a82e5fc4bcb087dd03b75e8eb7e
Olivia <olivia.bergaglia@gmail.com>
2024-11-26 19:13:53 +0100

    getting form methods out of header

---
+++ front/src/app/form/form.component.ts
@@ -0,0 +1,79 @@
+import { AsyncPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Observable, of } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class Service {
+  constructor(private http: HttpClient) {}
+
+  opts = [];
+
+  getData() {
+    return this.opts.length
+      ? of(this.opts)
+      : this.http
+          .get<any>('https://jsonplaceholder.typicode.com/users')
+          .pipe(tap((data) => (this.opts = data)));
+  }
+}
+
+/**
+ * @title Simple autocomplete
+ */
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
+  templateUrl: 'form.component.html',
+  styleUrls: ['form.component.scss'],
+})
+export class Form {
+  myControl = new FormControl();
+  options = [];
+  filteredOptions: Observable<any[]>;
+
+  constructor(private service: Service) {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((val) => {
+        return this.filter(val || '');
+      })
+    );
+  }
+
+  filter(val: string): Observable<any[]> {
+    // call the service which makes the http-request
+    return this.service.getData().pipe(
+      map((response) =>
+        response.filter((option: any) => {
+          return option.name.toLowerCase().indexOf(val.toLowerCase()) === 0;
+        })
+      )
+    );
+  }
+}
--- front/src/app/header/header.component.ts
+++ front/src/app/header/header.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule, MatFormFieldControl } from '@angular/material/form-field';
 import { CityData, FetchCityService } from '../service/fetchCity.service';
 import { FetchCompaniesDataService } from '../service/fetchCompaniesData.service';
 import { MapService } from '../service/map.service';
-import { Observable, of } from 'rxjs';
-import { debounceTime, distinctUntilChanged, map, startWith, switchMap, tap } from 'rxjs/operators';
-import { AsyncPipe } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
-import { HttpClient } from '@angular/common/http';
+import { Form } from '../form/form.component';
 
 
 export interface SingleCompanyData {
@@ -24,26 +17,16 @@ export interface SingleCompanyData {
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    MatAutocompleteModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AsyncPipe,
-  ],
+  imports: [Form],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 
 export class HeaderComponent {
-  myControl = new FormControl();
-
 
   private debounceTimeout!: any;
   private searchDelay = 25; // ms
   selectedCities: CityData[] = [];
-  selectedCity: CityData | null = null;
   citiesSuggestedArray: CityData[] = [];
   companiesFetched: SingleCompanyData[] = [];
 
@@ -51,7 +34,7 @@ export class HeaderComponent {
     private fetchCityService: FetchCityService,
     private fetchCompaniesDataService: FetchCompaniesDataService,
     private mapService: MapService,
-
+    // private form: Form
   ) { }
 
 
@@ -70,12 +53,12 @@ export class HeaderComponent {
         error: (err: Error) => console.error('Erreur :', err),
       });
     }, this.searchDelay);
-    
+
   }
 
   selectCity(city: CityData) {
     const cityAlreadySelected = this.selectedCities.some(selectedCity => selectedCity.code === city.code);
-    
+
     if (cityAlreadySelected) {
       this.selectedCities = this.selectedCities.filter(selectedCity => selectedCity.code !== city.code);
     } else {
@@ -89,7 +72,7 @@ export class HeaderComponent {
       next: (companiesData) => {
         this.companiesFetched = this.fetchCompaniesDataService.parseEstablishments(companiesData);
         console.log(this.companiesFetched);
-        
+
         this.mapService.addMarkers(this.companiesFetched);
       },
       error: (err) => console.error('Erreur lors de la récupération des données :', err),
@@ -98,49 +81,4 @@ export class HeaderComponent {
 
   // TODO :
   // timeout -> remplacer par un truc (un gars qui s'appel lesh de rxjs (demander a nico je sais plus))
-}
-
-/**
- * @title Simple autocomplete
- */
-export class AutocompleteSimpleExample {
-  myControl = new FormControl();
-  options = [];
-  filteredOptions: Observable<any[]>;
-  
-  constructor(
-    private fetchCityService: FetchCityService,
-    private http: HttpClient
-  ) {
-
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      debounceTime(400),
-      distinctUntilChanged(),
-      switchMap((val) => {
-        console.log("this.myControl.value")
-        return this.filter(val || '');
-      })
-    );
-  }
-
-  filter(val: string): Observable<any[]> {
-    // call the service which makes the http-request
-    return this.getData().pipe(
-      map((response) =>
-        response.filter((option: any) => {
-          return option.name.toLowerCase().indexOf(val.toLowerCase()) === 0;
-        })
-      )
-    );
-  }
-
-  opts = [];
-  getData() {
-    return this.opts.length
-      ? of(this.opts)
-      : this.http
-          .get<any>('https://jsonplaceholder.typicode.com/users')
-          .pipe(tap((data) => (this.opts = data)));
-  }
 }
