ea8e41981130e4e7aef731542ffea7e9e0ef566f
Olivia <olivia.bergaglia@gmail.com>
2024-11-28 19:27:59 +0100

    Init component

---
+++ front/src/app/nafcode-form/nafcode-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nafcode-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './nafcode-form.component.html',
+  styleUrl: './nafcode-form.component.scss'
+})
+export class NafcodeFormComponent {
+
+}
